52178123ec0817d82c56607630c842559fdd6dba
Lisheng Zheng <zhenglisheng@caicloud.io>
2019-03-20 10:20:15 +0800

    fix(delete): don't set cleanAzure to true when an error occurred

--- providers/azure/azure.go
+++ providers/azure/azure.go
@@ -416,13 +416,15 @@ func (l *AzureProvider) patchLoadBalancerAzureStatus(lb *lbapi.LoadBalancer, pha
 
 // clean up azure lb info and make oldAzureProvider nil
 func (l *AzureProvider) cleanupAzureLB(lb *lbapi.LoadBalancer, deleteLB bool) error {
+	log.Info("start clean up ")
 	if l.cleanAzure {
 		log.Info("azure loadbalancer is already clean...")
 		return nil
 	}
-	l.cleanAzure = true
+
 	if l.oldAzureProvider == nil || len(l.oldAzureProvider.Name) == 0 {
 		log.Errorf("old azure info nil")
+		l.cleanAzure = true
 		return nil
 	}
 	c, err := client.NewClient(&l.storeLister)
@@ -459,7 +461,11 @@ func (l *AzureProvider) cleanupAzureLB(lb *lbapi.LoadBalancer, deleteLB bool) er
 			l.patchLoadBalancerAzureStatus(lb, lbapi.AzureErrorPhase, err)
 			return err
 		}
-		return l.patachFinalizersAndStatus(lb, deleteLB)
+		err = l.patachFinalizersAndStatus(lb, deleteLB)
+		if err == nil {
+			l.cleanAzure = true
+		}
+		return err
 	}
 	log.Infof("delete azure lb group %s name %s", l.oldAzureProvider.ResourceGroupName, l.oldAzureProvider.Name)
 	err = c.LoadBalancer.Delete(context.TODO(), l.oldAzureProvider.ResourceGroupName, l.oldAzureProvider.Name)
@@ -467,7 +473,11 @@ func (l *AzureProvider) cleanupAzureLB(lb *lbapi.LoadBalancer, deleteLB bool) er
 	if err != nil {
 		return err
 	}
-	return l.patachFinalizersAndStatus(lb, deleteLB)
+	err = l.patachFinalizersAndStatus(lb, deleteLB)
+	if err == nil {
+		l.cleanAzure = true
+	}
+	return err
 }
 
 func cleanUpSecurityGroup(c *client.Client, groupName, lbName string) error {
